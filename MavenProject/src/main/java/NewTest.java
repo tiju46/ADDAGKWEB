04f410b40c54224d9eac82357f8b2e2b22a56de8
Tiju Thomas <thomas@3five8.com>
2019-08-08 15:46:34 +0530

    test commit

--- MavenProject/src/main/java/NewTest.java
+++ MavenProject/src/main/java/NewTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -20,9 +21,10 @@ public class NewTest {
 	  System.out.println("launching Mozilla FireFox browser");
 	  //System.setProperty("webdriver.gecko.driver","C:\\Users\\tiju thomas\\Downloads\\geckodriver-v0.24.0-win64\\geckodriver.exe");
 	  System.setProperty("webdriver.gecko.driver", "/home/testing/Downloads/geckodriver-v0.24.0-linux64/geckodriver");
-	  // FirefoxOptions options = new FirefoxOptions();
 	  //options.setCapability("marionette", false);
-	  driver = new FirefoxDriver();
+	  FirefoxOptions options = new FirefoxOptions();
+      options.setHeadless(true);
+      driver = new FirefoxDriver(options);
   }
   
     
