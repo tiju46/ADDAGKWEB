bdc18a03a296aad020d213876606e6164c74088c
Tiju Thomas <thomas@3five8.com>
2019-08-16 12:15:40 +0530

    test commit

--- MavenProject/src/main/java/NewTest.java
+++ MavenProject/src/main/java/NewTest.java
@@ -19,7 +19,7 @@ public class NewTest {
    
 	  System.out.println("launching Mozilla FireFox browser");
 	  //System.setProperty("webdriver.gecko.driver","C:\\Users\\tiju thomas\\Downloads\\geckodriver-v0.24.0-win64\\geckodriver.exe");
-	 System.setProperty("webdriver.gecko.driver", "/home/testing/Downloads/geckodriver-v0.24.0-linux64/geckodriver");
+	  System.setProperty("webdriver.gecko.driver", "/home/testing/Downloads/geckodriver-v0.24.0-linux64/geckodriver");
 	  //options.setCapability("marionette", false);
 	  FirefoxOptions options = new FirefoxOptions();
       options.setHeadless(true);
@@ -39,7 +39,7 @@ public class NewTest {
         public void CheckIn() throws InterruptedException {
 	    driver.findElement(By.xpath("//ng-multiselect-dropdown[@id='unit_number']")).click();
 	    driver.findElement(By.xpath("//div[contains(text(),'Block 1-002')]")).click();
-	    driver.findElement(By.xpath("//tr"));	
+	    //driver.findElement(By.xpath("//tr"));	
 	    driver.findElement(By.xpath("//button[@type='button'][contains(text(),'CHECK IN')]")).click();
 	    Thread.sleep(5000);
         driver.findElement(By.xpath("//ng-multiselect-dropdown[@id='reason']")).click();
